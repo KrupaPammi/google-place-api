912bc8b4c1a3ac3e9f031f6b5d76cd7e1bbf6fd6
krupa <k.pammi@comicrelief.com>
2018-07-02 13:31:55 +0100

    Created ResuableMethods class to convert string to json/xml format

--- src/AddPlaceTestInXML.java
+++ src/AddPlaceTestInXML.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
+import files.ReusableMethods;
 
 import org.testng.annotations.Test;
 
@@ -17,37 +18,30 @@ import io.restassured.response.Response;
 public class AddPlaceTestInXML {
 
 	Properties prop = new Properties();
-	
+
 	@Test
 	public void postData() throws IOException {
-	
-		FileInputStream fis = new FileInputStream("../RestAPISampleProject/src/files/env.properties");
-		
+
+		FileInputStream fis = new FileInputStream("../google-place-api/src/files/env.properties");
+
 		prop.load(fis);
-		
-	String postData = GenerateStringFromResource("../RestAPISampleProject/src/files/PostData.xml");
-		
-	RestAssured.baseURI=prop.getProperty("HOST");
-	
-	Response resp = given().
-	queryParam("key", prop.getProperty("KEY")).
-	body(postData).
-	when().
-	post("/maps/api/place/add/xml").
-	then().assertThat().statusCode(200).and().contentType(ContentType.XML).
-	
-	extract().response();
-  
-	String res=resp.asString();
-	System.out.println(res);
-	
-	XmlPath x = new XmlPath(res);
-	System.out.println(x.get("PlaceAddResponse.place_id"));
+
+		String postData = GenerateStringFromResource("../google-place-api/src/files/PostData.xml");
+
+		RestAssured.baseURI = prop.getProperty("HOST");
+
+		Response resp = given().queryParam("key", prop.getProperty("KEY")).body(postData).when()
+				.post("/maps/api/place/add/xml").then().assertThat().statusCode(200).and().contentType(ContentType.XML).
+
+				extract().response();
+
+		// using reusable method to convert the raw to string and string to xml
+		XmlPath x = ReusableMethods.rawToXml(resp);
+
+		System.out.println(x.get("PlaceAddResponse.place_id"));
 	}
-	
-	
-	public static String GenerateStringFromResource(String path) throws IOException
-	{
+
+	public static String GenerateStringFromResource(String path) throws IOException {
 		return new String(Files.readAllBytes(Paths.get(path)));
 	}
 }
