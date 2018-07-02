c64f31bda39f5fd5a3efd819413e274f331483f9
krupa <k.pammi@comicrelief.com>
2018-07-02 13:59:25 +0100

    Fix BaseTest

--- src/BaseTest.java
+++ src/BaseTest.java
@@ -1,12 +1,18 @@
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.testng.annotations.BeforeTest;
+
+import files.resources;
 
 
 public class BaseTest {
@@ -24,20 +30,18 @@ public class BaseTest {
 		RestAssured.baseURI=prop.getProperty("HOST");
 		
 		given().
-//		        param("location","-33.8670522,151.1957362").
-//		        param("radius","1500").
-		        queryParam("key",prop.getProperty("KEY"));
-//		when().
-//				get(resources.getNearbySearchPlace()).
-//		then().assertThat().statusCode(200).and().contentType(ContentType.JSON).
-//		//1st validation - checking status code
-//		//2nd validation - checking if the ContentType is in Json format or not
-//		
-//		//3rd validation
-//		and().body("results[0].name",equalTo("Sydney")).and().body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM"))
-//		//4th validation
-//		.and().header("server","scaffolding on HTTPServer2");
+		        param("location","-33.8670522,151.1957362").
+		        param("radius","1500").
+		        queryParam("key",prop.getProperty("KEY")).
+		when().
+				get(resources.getNearbySearchPlace()).
+		then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+		//1st validation - checking status code
+		//2nd validation - checking if the ContentType is in Json format or not		
+		//3rd validation - checking the body
+		body("results[0].name",equalTo("Sydney")).and().body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM"))
+		//4th validation - checking the header
+		.and().header("server","scaffolding on HTTPServer2");
 	}
-
 
 }
